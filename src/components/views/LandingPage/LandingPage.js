0faa141bd56791f10fa9182208182197e2965603
Se Joon Kim <sejoondev@gmail.com>
2020-06-12 13:32:10 +0900

    initial commit

---
+++ src/components/views/LandingPage/LandingPage.js
@@ -0,0 +1,34 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+
+function LandingPage(props) {
+  useEffect(() => {
+    axios.get("/api/hello").then((response) => console.log(response.data));
+  }, []);
+
+  const onClickHandler = () => {
+    axios.get(`/api/users/logout`).then((response) => {
+      if (response.data.success) {
+        props.history.push("/login");
+      } else {
+        alert("Failed to logout.");
+      }
+    });
+  };
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <h2>Landing Page</h2>
+      <button onClick={onClickHandler}>Log out</button>
+    </div>
+  );
+}
+
+export default LandingPage;

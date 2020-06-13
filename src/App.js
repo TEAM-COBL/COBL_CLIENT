5adba07da5367ef7b5501c077da268c59dd3269b
Se Joon Kim <sejoondev@gmail.com>
2020-06-13 09:58:13 +0900

    feat: Completed Frontend
    Added many pages for the app CoBl
      - login
      - landing page
      - donation map
      - donation list
      - donation detail
      - finished

--- src/App.js
+++ src/App.js
@@ -5,10 +5,18 @@ import LandingPage from "./components/views/LandingPage/LandingPage";
 import LoginPage from "./components/views/LoginPage/LoginPage";
 import RegisterPage from "./components/views/RegisterPage/RegisterPage";
 import Auth from './hoc/auth'
+import NavBar from "./components/views/NavBar/NavBar";
+import MyPage from "./components/views/MyPage/MyPage";
+import DonationMapPage from "./components/views/DonationMapPage/DonationMapPage"
+import DonationInstitutionListPage from "./components/views/DonationInstitutionListPage/DonationInstitutionListPage";
+import RedCross from "./components/views/DonationInstitutionDetailPage/RedCross"
+import GreenUmbrella from "./components/views/DonationInstitutionDetailPage/GreenUmbrella"
+import FinishedPage from "./components/views/FinishedPage/FinishedPage";
 function App() {
   return (
     <Router>
       <div>
+        <NavBar />
         {/*
           A <Switch> looks through all its children <Route>
           elements and renders the first one whose path
@@ -17,9 +25,15 @@ function App() {
           of them to render at a time
         */}
         <Switch>
-          <Route exact path="/" component={Auth(LandingPage, null, )} />
-          <Route exact path="/login" component={LoginPage} />
-          <Route exact path="/register" component={RegisterPage} />
+          <Route exact path="/" component={Auth(LandingPage, null)} />
+          <Route exact path="/login" component={Auth(LoginPage, false)} />
+          <Route exact path="/register" component={Auth(RegisterPage, false)} />
+          <Route exact path="/mypage" component={Auth(MyPage, true)} />
+          <Route exact path="/donationmap" component={Auth(DonationMapPage, null)} />
+          <Route exact path="/institutionlist" component={Auth(DonationInstitutionListPage, null)} />
+          <Route exact path="/redcross" component={Auth(RedCross, null)} />
+          <Route exact path="/greenumbrella" component={Auth(GreenUmbrella, null)} />
+          <Route exact path="/finished" component={Auth(FinishedPage, null)} />
         </Switch>
       </div>
     </Router>
--- src/hoc/auth.js
+++ src/hoc/auth.js
@@ -9,8 +9,26 @@ export default function (SpecificComponent, option, adminRoute = null) {
         useEffect(() => {
             dispatch(auth()).then(response => {
                 console.log(response)
+                
+                // Did not login
+                if (!response.payload.isAuth) {
+                    if (option) {
+                        props.history.push('/login')
+                    }
+                } else { // already logged in
+                    if (adminRoute && !response.payload.isAdmin) {
+                        props.history.push('/')
+                    }
+                    else {
+                        if (option === false)
+                            props.history.push('/')
+                    }
+                }
             })
         }, [])
+        return (
+            <SpecificComponent/>
+        )
     }
 
     return AuthenticationCheck
